50c76acefc295d18b7a34a037dbd23d55b10d45b
Clay Lindstrom <clayjlindstrom@gmail.com>
2022-06-03 19:16:11 -0600

    Starting Area Spawns/Camera Script Created!

---
+++ KingCore/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    //to track the location of the player!
+    private Transform playerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        //we'll likely just have the camera start at the player's position,
+        Vector3 startingPos = transform.position;
+        startingPos.y = playerPos.position.y;
+        transform.position = startingPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //we'll likely just have the camera start at the player's position,
+        //and then we'll have it gradually move upward (one unit per second) as the game goes on.
+        //so long as the player doesn't die.
+        transform.Translate(Vector3.up * Time.deltaTime);
+    }
+}
--- KingCore/Assets/Scripts/LevelGeneratorScript.cs
+++ KingCore/Assets/Scripts/LevelGeneratorScript.cs
@@ -18,7 +18,8 @@ public class LevelGeneratorScript : MonoBehaviour
 
         //Instantiate(wallTile, Vector3.right, Quaternion.identity);
         //MakeWall(Vector3.left * tileLength * 4 - Vector3.left*tileLength/2, 8);
-        MakeFloor(Vector3.down * tileLength * 4, 4);
+        MakeStartingFloor();
+        //MakeFloor(Vector3.down * tileLength * 4, 4);
     }
 
     // Update is called once per frame
@@ -35,7 +36,6 @@ public class LevelGeneratorScript : MonoBehaviour
             Instantiate(wallTile, wallLoc + Vector3.up * tileLength * i, Quaternion.identity);
         }
     }
-
     //This is to make the floor at "floorLoc" location and "length" blocks long
     public void MakeFloor(Vector3 wallLoc, int length){
         //if there is an even number of blocks
@@ -62,6 +62,19 @@ public class LevelGeneratorScript : MonoBehaviour
 
     //this will make the floor, and three platforms for the player to jump across.
     public void MakeStartingFloor(){
-
+        //first, we make the floor 6 blocks long and 4 units down from the center
+        MakeFloor(Vector3.down * tileLength * 3, 6);
+        //then we make two walls on each side of the floor.
+        MakeWall(Vector3.right *tileLength * 3.5f
+            + Vector3.down * tileLength * 3, 8);
+        MakeWall(Vector3.left*tileLength * 3.5f
+            + Vector3.down * tileLength * 3, 8);
+        //now we add three simple platforms for the player to jump onto
+        //one at the bottom right, 3 blocks above the floor,
+        MakeFloor(Vector3.right * tileLength * 2, 2);
+        //one in the middle, 3 blocks above the last
+        MakeFloor(Vector3.up * tileLength * 3, 2);
+        //and one more 3 blocks up, but to the left;
+        MakeFloor(new Vector3(-2, 6, 0) * tileLength, 2);
     }
 }
