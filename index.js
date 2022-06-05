8d9631880d055d0a71f6e25c7e18eb2b6e2ca34a
kctolli <tolligithub@gmail.com>
2022-06-04 23:12:10 -0600

    basic frame and image

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 let app = {};
 
 app.index = {
+    date: new Date(),
+    year: new Date().getFullYear(),
+    underTen: function(x){return (x < 10 ? x = `0${x}` : x);},
+    datestamp: function(){
+        return `${app.index.year}${app.index.underTen(app.index.date.getMonth() + 1)}${app.index.underTen(app.index.date.getDate())}`;
+    }, 
     footer: function(){
-        $(`footer`).html(`&copy; ${(new Date()).getFullYear()} Game Jam -- Parkour`); 
+        $(`footer`).html(`&copy; ${app.index.year} Game Jam -- Parkour`); 
     },
+    init: function(){
+        app.index.footer();
+    }
 };
+
+
 
 $(document).ready(function(){
     app.index.footer();
+
+    (firedb.ref(app.index.datestamp())).on(`value`, function(snapshot){
+        let data = snapshot.val();
+        console.log(data);
+    });
 });
